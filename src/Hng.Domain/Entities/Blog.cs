1ef1920e7433a8762fc2f908fe9454e68ee3de27
Abdulquddus Nuhu <abdulquddusnuhu@gmail.com>
2024-07-13 22:37:28 +0100

    fix

--- src/Hng.Domain/Entities/Blog.cs
+++ src/Hng.Domain/Entities/Blog.cs
@@ -14,5 +14,6 @@ namespace Hng.Domain.Entities
         public string Body { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
     }
 }
